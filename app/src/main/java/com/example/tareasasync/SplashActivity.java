f8a112c64ba13ddd2c74e7a890dff0cf509fa88c
Enrique Fernandez <enriquefernandezchacon@gmail.com>
2023-01-23 19:55:02 +0100

    Modificacion de splash activity

--- app/src/main/java/com/example/tareasasync/SplashActivity.java
+++ app/src/main/java/com/example/tareasasync/SplashActivity.java
@@ -6,20 +6,79 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.widget.Toast;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class SplashActivity extends AppCompatActivity {
 
     private int espera = 4000;
     SplashActivity.AsyncTaskRunner runner;
 
+    //
+    private String resp;
+    private ProgressDialog progressDialog;
+
+    //
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        runner = new SplashActivity.AsyncTaskRunner();
-        runner.execute(String.valueOf(espera));
+        ExecutorService service = Executors.newSingleThreadExecutor();
+
+        Handler handler = new Handler(Looper.getMainLooper());
+
+        service.execute(new Runnable() {
+            @Override
+            public void run() {
+                //onPreExecute
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressDialog = ProgressDialog.show(SplashActivity.this,
+                                "Dialogo de Progreso",
+                                "Conectando al servidor...");
+                    }
+                });
+                //doing background
+                //publishProgress("Reciviendo datos..."); // Calls onProgressUpdate()
+
+                Toast.makeText(SplashActivity.this, "Reciviendo datos...", Toast.LENGTH_SHORT).show();
+
+                resp = tareaPesada(espera);
+                //doPostExecute
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressDialog.dismiss();
+
+                        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+            }
+        });
+        //runner = new SplashActivity.AsyncTaskRunner();
+        //runner.execute(String.valueOf(espera));
+    }
+
+    protected String tareaPesada(int tiempo){
+        try {
+            Thread.sleep(tiempo);
+            return "Dormido durante " + tiempo + " seconds";
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return e.getMessage();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
     }
 
 
